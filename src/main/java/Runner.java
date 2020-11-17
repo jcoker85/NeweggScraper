c302e190125a21934a6a5d2b2063a7e993d9bbe0
pulu <puluceno@gmail.com>
2020-11-16 23:04:32 -0600

    Refactored a good part of the java part

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,14 +1,13 @@
+import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 
 import static java.lang.Thread.sleep;
 
@@ -17,10 +16,12 @@ public class Runner {
     private static FileHandler fh;
     private static BufferedInputStream bufferedInputStream;
     private static AudioInputStream audioInputStream;
+    private static final Logger logger = Logger.getLogger("pooplog");
 
     public static void main(String[] args) {
-
-        Logger logger = Logger.getLogger("pooplog");
+        List<String> scrape1 = new ArrayList<>();
+        List<String> scrape2 = new ArrayList<>();
+        List<String> scrape3 = new ArrayList<>();
 
         try {
             fh = new FileHandler("poop.log");
@@ -30,102 +31,41 @@ public class Runner {
 
             Scraper scraper = new Scraper();
 
-
             while (true) {
-                ArrayList<String> urls = scraper.getUrls();
-                if (scraper.keepScrapin) {
-                    if (urls.size() > 0) {
-                        ArrayList<String> scrape1 = new ArrayList<String>();
-                        try {
-                            if (scraper.bestBuyMode) {
-                                scrape1 = ScraperHelper.scrapeBestBuy(urls.get(0), false);
-                            } else {
-                                if (scraper.scrapeIndividual)
-                                    scrape1 = ScraperHelper.scrapeIndividual(urls.get(0), false);
-                                else
-                                    scrape1 = ScraperHelper.scrape(urls.get(0), false);
-                            }
-                        } catch (Exception e) {
-                            logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
+                List<String> urls = scraper.getUrls();
+                if (scraper.keepScraping) {
+                    try {
+                        if (scraper.scrapeIndividual) {
+                            scrape1 = ScraperHelper.scrapeIndividual(urls.get(0), false);
+                            scrape2 = ScraperHelper.scrapeIndividual(urls.get(1), false);
+                            scrape3 = ScraperHelper.scrapeIndividual(urls.get(2), false);
+                        } else {
+                            scrape1 = ScraperHelper.scrape(urls.get(0), false);
+                            scrape2 = ScraperHelper.scrape(urls.get(1), false);
+                            scrape3 = ScraperHelper.scrape(urls.get(2), false);
                         }
-                        if (scrape1.size() > 0) {
-                            for (String item : scrape1) {
-                                logger.info(item.split("‽")[0] + " IN STOCK\r\n      " + item.split("‽")[1]);
-                            }
-                            if (!scraper.muteSound) {
-                                bufferedInputStream = new BufferedInputStream(Objects.requireNonNull(Runner.class.getClassLoader().getResourceAsStream("alert.wav")));
-                                audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream);
-                                Clip clip = AudioSystem.getClip();
-                                clip.open(audioInputStream);
-                                clip.start();
-                            }
-                        }
-                        scraper.set3080Content(scrape1);
+                    } catch (Exception e) {
+                        logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
                     }
-
-                    if (urls.size() > 1) {
-                        ArrayList<String> scrape2 = new ArrayList<String>();
-                        try {
-                            if (scraper.bestBuyMode) {
-                                scrape2 = ScraperHelper.scrapeBestBuy(urls.get(1), false);
-                            } else {
-                                if (scraper.scrapeIndividual)
-                                    scrape2 = ScraperHelper.scrapeIndividual(urls.get(1), false);
-                                else
-                                    scrape2 = ScraperHelper.scrape(urls.get(1), false);
-                            }
-                        } catch (Exception e) {
-                            logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
-                        }
-                        if (scrape2.size() > 0) {
-                            for (String item : scrape2) {
-                                logger.info(item.split("‽")[0] + " IN STOCK\r\n      " + item.split("‽")[1]);
-                            }
-                            if (!scraper.muteSound) {
-                                bufferedInputStream = new BufferedInputStream(Objects.requireNonNull(Runner.class.getClassLoader().getResourceAsStream("alert.wav")));
-                                audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream);
-                                Clip clip = AudioSystem.getClip();
-                                clip.open(audioInputStream);
-                                clip.start();
-                            }
-                        }
-                        scraper.set3090Content(scrape2);
-                    }
+                    startAlert(scraper, scrape1, scrape2, scrape3);
                 } else {
-                    if (urls.size() > 0) {
-                        ArrayList<String> scrape1 = new ArrayList<String>();
-                        try {
-                            if (scraper.bestBuyMode) {
-                                scrape1 = ScraperHelper.scrapeBestBuy(urls.get(0), true);
-                            } else {
-                                if (scraper.scrapeIndividual)
-                                    scrape1 = ScraperHelper.scrapeIndividual(urls.get(0), true);
-                                else
-                                    scrape1 = ScraperHelper.scrape(urls.get(0), true);
-                            }
-                        } catch (Exception e) {
-                            logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
+                    try {
+                        if (scraper.scrapeIndividual) {
+                            scrape1 = ScraperHelper.scrapeIndividual(urls.get(0), true);
+                            scrape2 = ScraperHelper.scrapeIndividual(urls.get(1), true);
+                            scrape3 = ScraperHelper.scrapeIndividual(urls.get(2), true);
+                        } else {
+                            scrape1 = ScraperHelper.scrape(urls.get(0), true);
+                            scrape2 = ScraperHelper.scrape(urls.get(1), true);
+                            scrape3 = ScraperHelper.scrape(urls.get(2), true);
                         }
-                        scraper.set3080Content(scrape1);
-                    }
-
-                    if (urls.size() > 1) {
-                        ArrayList<String> scrape2 = new ArrayList<String>();
-                        try {
-                            if (scraper.bestBuyMode) {
-                                scrape2 = ScraperHelper.scrapeBestBuy(urls.get(1), true);
-                            } else {
-                                if (scraper.scrapeIndividual)
-                                    scrape2 = ScraperHelper.scrapeIndividual(urls.get(1), true);
-                                else
-                                    scrape2 = ScraperHelper.scrape(urls.get(1), true);
-                            }
-                        } catch (Exception e) {
-                            logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
-                        }
-                        scraper.set3090Content(scrape2);
+                    } catch (Exception e) {
+                        logger.warning("Groovy Error detected: " + e.toString() + "\r\n         Stacktrace: " + Arrays.toString(e.getStackTrace()));
                     }
                 }
+                scraper.setNewegg3080Content(scrape1);
+                scraper.setNeweggCpuContent(scrape2);
+                scraper.setNeweggPs5Content(scrape3);
                 sleep(5000);
             }
         } catch (Exception e) {
@@ -135,7 +75,22 @@ public class Runner {
         }
     }
 
-    public static void closer()  {
+    private static void startAlert(Scraper scraper, List<String>... scrapeResult) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        for (List<String> result : scrapeResult) {
+            for (String item : result) {
+                logger.info(item.split("‽")[0] + " IN STOCK\r\n  " + item.split("‽")[1]);
+            }
+            if (!scraper.muteSound) {
+                bufferedInputStream = new BufferedInputStream(Objects.requireNonNull(Runner.class.getClassLoader().getResourceAsStream("alert.wav")));
+                audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+            }
+        }
+    }
+
+    public static void closer() {
         try {
             fh.close();
             if (bufferedInputStream != null)
--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,147 +1,119 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Desktop;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.Timer;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
-
-import org.apache.commons.lang3.StringEscapeUtils;
-
+import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Scraper {
 
-    public boolean keepScrapin = false;
+    public boolean keepScraping = false;
     public boolean scrapeIndividual = false;
     public boolean muteSound = false;
-    public boolean bestBuyMode = false;
 
-    private ArrayList<String> data3080 = new ArrayList<String>();
-    private ArrayList<String> data3090 = new ArrayList<String>();
-
-    private UrlTextPane pane3080 = new UrlTextPane();
-    private UrlTextPane pane3090 = new UrlTextPane();
-
-    //private JTextField url3080 = new JTextField("https://www.bestbuy.com/site/searchpage.jsp?st=3080+rtx");
-    private JTextField url3080 = new JTextField("https://www.newegg.com/p/pl?d=rtx+3080&N=100007709&isdeptsrh=1");
-    //private JTextField url3090 = new JTextField("https://www.bestbuy.com/site/searchpage.jsp?st=3090+rtx");
-    private JTextField url3090 = new JTextField("https://www.newegg.com/p/pl?d=rtx+3090&N=100007709&isdeptsrh=1");
-
-    private JButton enableScrape = new JButton("In/Out Of Stock");
-    private JButton enableBestBuyMode = new JButton("Best Buy Mode");
-    private JButton enableIndividual = new JButton("Individual Pages");
-    private JButton enableMute = new JButton("Mute Sound");
+    private List<String> data3080 = new ArrayList<>();
+    private List<String> dataCpu = new ArrayList<>();
+    private List<String> dataPS5 = new ArrayList<>();
 
     JFrame frame = new JFrame();
+    private final UrlTextPane pane3080 = new UrlTextPane();
+    private final UrlTextPane paneCpu = new UrlTextPane();
+    private final UrlTextPane panePS5 = new UrlTextPane();
+
+    private final JTextField urlNewegg3080 = new JTextField("https://www.newegg.com/p/pl?d=rtx+3080&N=100007709&isdeptsrh=1");
+    private final JTextField urlNeweggCpu = new JTextField("https://www.newegg.com/p/pl?d=ryzen+5000&N=50001028");
+    private final JTextField urlNeweggPS5 = new JTextField("https://www.newegg.com/p/pl?d=playstation+5&N=101696840&isdeptsrh=1");
+    private final JTextField urlBestbuy3080 = new JTextField("https://www.bestbuy.com/site/searchpage.jsp?st=3080+rtx");
+    private final JTextField urlBestbuyCpu = new JTextField("https://www.bestbuy.com/site/searchpage.jsp?qp=brand_facet%3DBrand~AMD&st=amd%20ryzen%205800%205900");
+    private final JTextField urlBestbuyPS5 = new JTextField("https://www.bestbuy.com/site/playstation-5/ps5-consoles/pcmcat1587395025973.c?id=pcmcat1587395025973");
+
+    private final JButton enableScrape = new JButton("In/Out Of Stock");
+    private final JButton enableIndividual = new JButton("Individual Pages");
+    private final JButton enableMute = new JButton("Mute Sound");
 
     public Scraper() {
 
         JPanel panel3080 = new JPanel();
-        JPanel panel3090 = new JPanel();
+        JPanel panelCpu = new JPanel();
+        JPanel panelPs5 = new JPanel();
 
-        Timer timer = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                pane3080.setText(convertText(data3080));
-                pane3090.setText(convertText(data3090));
-            }
+        Timer timer = new Timer(5000, e -> {
+            pane3080.setText(convertText(data3080));
+            paneCpu.setText(convertText(dataCpu));
+            panePS5.setText(convertText(dataPS5));
         });
         timer.start();
 
         enableScrape.setBackground(Color.RED);
-        enableScrape.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                if (keepScrapin) {
-                    keepScrapin = false;
-                    enableScrape.setBackground(Color.RED);
-                } else {
-                    keepScrapin = true;
-                    enableScrape.setBackground(Color.GREEN);
-                }
-                setUrls(url3080.getText(), url3090.getText());
+        enableScrape.addActionListener(arg0 -> {
+            if (keepScraping) {
+                keepScraping = false;
+                enableScrape.setBackground(Color.RED);
+            } else {
+                keepScraping = true;
+                enableScrape.setBackground(Color.GREEN);
             }
-        });
-
-        enableBestBuyMode.setBackground(Color.RED);
-        enableBestBuyMode.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                if (bestBuyMode) {
-                    bestBuyMode = false;
-                    enableBestBuyMode.setBackground(Color.RED);
-                } else {
-                    bestBuyMode = true;
-                    enableBestBuyMode.setBackground(Color.GREEN);
-                }
-            }
+            setUrls(urlNewegg3080.getText(), urlNeweggCpu.getText(), urlNeweggPS5.getText());
         });
 
         enableIndividual.setBackground(Color.RED);
-        enableIndividual.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                if (scrapeIndividual) {
-                    scrapeIndividual = false;
-                    enableIndividual.setBackground(Color.RED);
-                } else {
-                    scrapeIndividual = true;
-                    enableIndividual.setBackground(Color.GREEN);
-                }
+        enableIndividual.addActionListener(arg0 -> {
+            if (scrapeIndividual) {
+                scrapeIndividual = false;
+                enableIndividual.setBackground(Color.RED);
+            } else {
+                scrapeIndividual = true;
+                enableIndividual.setBackground(Color.GREEN);
             }
         });
 
         enableMute.setBackground(Color.RED);
-        enableMute.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                if (muteSound) {
-                    muteSound = false;
-                    enableMute.setBackground(Color.RED);
-                } else {
-                    muteSound = true;
-                    enableMute.setBackground(Color.GREEN);
-                }
+        enableMute.addActionListener(arg0 -> {
+            if (muteSound) {
+                muteSound = false;
+                enableMute.setBackground(Color.RED);
+            } else {
+                muteSound = true;
+                enableMute.setBackground(Color.GREEN);
             }
         });
 
         JPanel mainContainer = new JPanel();
-        mainContainer.setLayout(new GridLayout(1, 2));
-        mainContainer.add(setup3080Panel(panel3080, pane3080));
-        mainContainer.add(setup3090Panel(panel3090, pane3090));
-        mainContainer.setPreferredSize(new Dimension(1000, 660));
+        mainContainer.setLayout(new GridLayout(1, 3));
+        mainContainer.add(new JScrollPane(setUpPanel(panel3080, pane3080, "RTX 3080 Tracker", "RTX 3080")));
+        mainContainer.add(new JScrollPane(setUpPanel(panelCpu, paneCpu, "Ryzen 5000 Series Tracker", "Ryzen 5000 Series")));
+        mainContainer.add(new JScrollPane(setUpPanel(panelPs5, panePS5, "Playstation 5 Tracker", "Playstation 5 Console")));
+        mainContainer.setPreferredSize(new Dimension(1900, 860));
+
+        JPanel urlContainer = new JPanel();
+        urlContainer.setLayout(new GridLayout(1, 3));
+        urlContainer.add(urlNewegg3080);
+        urlContainer.add(urlNeweggCpu);
+        urlContainer.add(urlNeweggPS5);
+        urlContainer.setPreferredSize(new Dimension(1900, 30));
+
         JPanel buttonContainer = new JPanel();
-        buttonContainer.setLayout(new GridLayout(3, 2));
-        buttonContainer.add(url3080);
-        buttonContainer.add(url3090);
+        buttonContainer.setLayout(new GridLayout(1, 3));
         buttonContainer.add(enableScrape);
-        buttonContainer.add(enableBestBuyMode);
         buttonContainer.add(enableIndividual);
         buttonContainer.add(enableMute);
-        buttonContainer.setPreferredSize(new Dimension(1000, 120));
+        buttonContainer.setPreferredSize(new Dimension(1900, 60));
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1030, 840));
-        frame.setLayout(new FlowLayout(0));
+        frame.setPreferredSize(new Dimension(1920, 1020));
+        frame.setLayout(new FlowLayout(FlowLayout.LEFT));
         frame.add(mainContainer, BorderLayout.NORTH);
+        frame.add(urlContainer, BorderLayout.SOUTH);
         frame.add(buttonContainer, BorderLayout.SOUTH);
-        frame.setTitle("TRACKERBOI 9000");
+        frame.setTitle("NE and BB Tracker");
         frame.pack();
-        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
@@ -152,61 +124,46 @@ public class Scraper {
         });
     }
 
-    public void set3080Content(ArrayList<String> data) {
+    public void setNewegg3080Content(List<String> data) {
         data3080 = data;
     }
 
-    public void set3090Content(ArrayList<String> data) {
-        data3090 = data;
+    public void setNeweggCpuContent(List<String> data) {
+        dataCpu = data;
     }
 
-    public void setUrls(String uri3080, String uri3090) {
-        url3080.setText(uri3080);
-        url3090.setText(uri3090);
+    public void setNeweggPs5Content(List<String> data) {
+        dataPS5 = data;
     }
 
-    public ArrayList<String> getUrls() {
-        ArrayList<String> ret = new ArrayList<String>();
-        ret.add(url3080.getText());
-        ret.add(url3090.getText());
+    public void setUrls(String uri3080, String uriCpu, String uriPs5) {
+        urlNewegg3080.setText(uri3080);
+        urlNeweggCpu.setText(uriCpu);
+        urlNeweggPS5.setText(uriPs5);
+    }
+
+    public List<String> getUrls() {
+        List<String> ret = new ArrayList<>();
+        ret.add(urlNewegg3080.getText());
+        ret.add(urlNeweggCpu.getText());
+        ret.add(urlNeweggPS5.getText());
         return ret;
     }
 
-    private Component setup3080Panel(JPanel panel3080, UrlTextPane pane3080) {
+    private Component setUpPanel(JPanel panel, UrlTextPane urlTextPane, String title, String subtitle) {
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(title);
+        titledBorder.setTitleJustification(TitledBorder.CENTER);
+        titledBorder.setTitleColor(Color.BLACK);
 
-        //Setup 3080 panel
-        TitledBorder title = BorderFactory.createTitledBorder("3080 Tracker");
-        title.setTitleJustification(TitledBorder.CENTER);
-        title.setTitleColor(Color.BLACK);
-        panel3080.setBorder(title); //Set title to the Panel
+        panel.setBorder(titledBorder); //Set title to the Panel
+        panel.setLayout(new BorderLayout());
+        urlTextPane.setBorder(BorderFactory.createTitledBorder(subtitle));
+        panel.add(urlTextPane, BorderLayout.NORTH);
 
-        panel3080.setLayout(new BorderLayout());
-
-        pane3080.setBorder(BorderFactory.createTitledBorder("3080"));
-
-        panel3080.add(pane3080, BorderLayout.NORTH);
-
-        return panel3080;
+        return panel;
     }
 
-    private Component setup3090Panel(JPanel panel3090, UrlTextPane pane3090) {
-
-        //Setup 3090 panel
-        TitledBorder title = BorderFactory.createTitledBorder("3090 Tracker");
-        title.setTitleJustification(TitledBorder.CENTER);
-        title.setTitleColor(Color.BLACK);
-        panel3090.setBorder(title); //Set title to the Panel
-
-        panel3090.setLayout(new BorderLayout());
-
-        pane3090.setBorder(BorderFactory.createTitledBorder("3090"));
-
-        panel3090.add(pane3090, BorderLayout.NORTH);
-
-        return panel3090;
-    }
-
-    private String convertText(ArrayList<String> data) {
+    private String convertText(List<String> data) {
 
         if (data.size() == 0) {
             return "";
@@ -219,13 +176,13 @@ public class Scraper {
         answer.append("<html><body>");
 
         for (String entry : data) {
-            String split[] = entry.split("‽");
+            String[] split = entry.split("‽");
             String content = StringEscapeUtils.escapeHtml4(split[1]).replace(" ", "%20");
             int lastIndex = 0;
             Matcher matcher = urlPattern.matcher(content);
             while (matcher.find()) {
                 //Append everything since last update to the url:
-                answer.append(content.substring(lastIndex, matcher.start()));
+                answer.append(content, lastIndex, matcher.start());
                 String url = content.substring(matcher.start(), matcher.end()).trim();
                 answer.append("<a href=\"" + url + "\">" + split[0] + "</a>");
                 lastIndex = matcher.end();
@@ -250,9 +207,7 @@ public class Scraper {
                 if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
                     try {
                         Desktop.getDesktop().browse(event.getURL().toURI());
-                    } catch (IOException e) {
-                        throw new RuntimeException("Can't open URL", e);
-                    } catch (URISyntaxException e) {
+                    } catch (IOException | URISyntaxException e) {
                         throw new RuntimeException("Can't open URL", e);
                     }
                 }
